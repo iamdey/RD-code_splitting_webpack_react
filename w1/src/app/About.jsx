c904116bb543a1bd62334ea0e03b39d1a26e9ac7
David Epely <esion99@gmail.com>
2017-05-10 18:28:49 +0200

    implement jquery & kikoo script

--- w1/src/app/About.jsx
+++ w1/src/app/About.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import jquery from 'jquery';
 
-const About = ({}) => {
-  return (<h1>About</h1>);
-};
+class About extends Component {
+  componentDidMount() {
+    setTimeout(() => jquery('#kikoo').html('Hey, I am an old school Kikoo script'), 3000);
+  }
 
-About.propTypes = {
-};
-About.defaultProps = {
-};
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>I'm a page with jQuery and it's shame, I assume it.</p>
+        <div id="kikoo"></div>
+      </div>
+    );  }
+}
 
 export default About;
