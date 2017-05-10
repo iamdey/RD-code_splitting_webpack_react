6cfcf1b4bbed7e1972f5d7b7e4082e386969540a
David Epely <esion99@gmail.com>
2017-05-10 18:21:18 +0200

    implement zxcvbn

--- w1/src/app/Signup.jsx
+++ w1/src/app/Signup.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import zxcvbn from 'zxcvbn';
 
-const Signup = ({}) => {
-  return (<h1>Signup</h1>);
-};
+class Signup extends Component {
+  static propTypes = {
+  };
+  static defaultProps = {
+  };
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      password: '',
+      passwordStrength: '',
+    };
+  }
 
-Signup.propTypes = {
-};
-Signup.defaultProps = {
-};
+  handleChange(ev) {
+    const password = ev.target.value;
+    const passwordStrength = zxcvbn(password).score;
+    this.setState({
+      password,
+      passwordStrength,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Signup</h1>
+        <form method="post">
+          <label>
+            <div>Password</div>
+            <div>
+              <input type="text" onChange={this.handleChange} value={this.state.password} />
+            </div>
+            <div>Password strength: {this.state.passwordStrength}</div>
+          </label>
+        </form>
+      </div>
+
+    );
+  }
+}
 
 export default Signup;
